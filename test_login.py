6311ba703551966e1fb42ddcfc7a7b7183fe6d7f
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-08 13:58:11 -0300

    versao 1

--- test_login.py
+++ test_login.py
@@ -8,22 +8,36 @@ driver = webdriver.Chrome()
 # Acessa a página de cadastro usando o caminho absoluto com o protocólo file://
 # Certifique-se de que o carrinho está apontando para um arquivo HTML específico
 
-driver.get("file:///C:/Users/lara_g_souza/TESTE_DE_SISTEMAS/login.html")
+driver.get("http://localhost:8080/TESTE_DE_SISTEMAS/login.html")
 
 # Preenche o campo Nome
-usuario_input = driver.find_element(By.ID, "usuario")
-usuario_input.send_keys("admin")
+# usuario_input = driver.find_element(By.ID, "usuario")
+# usuario_input.send_keys("admin")
 
 # Preenche o campo CPF
-senha_input = driver.find_element(By.ID, "senha")
-senha_input.send_keys("123456")
+# senha_input = driver.find_element(By.ID, "senha")
+# senha_input.send_keys("123456")
 
 # Clica no botão de cadastro
-login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-login_button.click()
+# login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+# login_button.click()
+
+# Preenche o campo Nome
+driver.find_element(By.ID, "usuario").send_keys("admin")
+
+# Preenche o campo CPF
+driver.find_element(By.ID, "senha").send_keys("123456")
+
+# Clica no botão de cadastro
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
 # Aguarda alguns segundos para verificar o resultado
 time.sleep(5)
 
+if "Cadastro de Cliente" in driver.page_source:
+    print("Login realizado com sucesso!")
+else:
+    print("Erro ao realizar o login.")
+
 # Fecha o navegador
-driver.quit()
+# driver.quit()
